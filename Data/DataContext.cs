c066128a160e44ed673ec824df89d220ff8d9be2
lakovin <kinolifenur@gmail.com>
2024-09-28 19:21:16 +0600

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using ItemApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ItemApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
